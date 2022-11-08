ee79a6f7014f1f78db6e4bb805cf68198aadd1be
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-07 19:42:45 -0600

    added image to title and styled

--- travel_journal/src/components/Header.jsx
+++ travel_journal/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import globe from '../assets/globe.png'
 
 export const Header = () => {
   return (
     <div className='header'>
-
-        <h3>my travel journal</h3>
+      <img src={globe} alt="earth globe" />
+      <h3>my travel journal</h3>
     </div>
   )
 }
