5300a14efdfda482ec980c40b3c30c28a697c2c2
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-08 12:22:57 -0600

    updated data file with new locations

--- travel_journal/src/data.js
+++ travel_journal/src/data.js
@@ -4,19 +4,39 @@ export default [
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        startDate: "12 Jan, 2022",
+        endDate: "24 Jan, 2022",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
         id: 2,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        title: "Crested Butte",
+        location: "Colorado",
+        googleMapsUrl: "https://www.google.com/maps/place/Crested+Butte,+CO+81224/@38.8668734,-106.977624,15z/data=!3m1!4b1!4m5!3m4!1s0x8740723e8a38d3e3:0x37006e19d5e7f39f!8m2!3d38.8697146!4d-106.9878231",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        description: "Crested Butte is a town in the Rocky Mountains of Colorado. Downhill ski and mountain-bike trails crisscross the slopes of Crested Butte Mountain Resort. There are Nordic ski routes around town.",
+        imageUrl: "https://images.unsplash.com/photo-1610141432408-d40aee2ec793?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNyZXN0ZWQlMjBidXR0ZSUyMGNvbG9yYWRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id: 3,
+        title: "Aonach Eagach",
+        location: "Scotland",
+        googleMapsUrl: "https://www.google.com/maps/place/Aonach+Eagach/@56.6666662,-5.0333333,15z/data=!3m1!4b1!4m5!3m4!1s0x488930f4142b9235:0x6fd94eeb41644c6c!8m2!3d56.6666667!4d-5.0333333!5m1!1e4",
+        startDate: "12 Jan, 2020",
+        endDate: "24 Jan, 2020",
+        description: "The Aonach Eagach is a large mountain ridge in the Scottish Highlands, marking the northern edge of Glen Coe. It stretches east–west for several miles and includes two Munro summits: Sgùrr nam Fiannaidh at 967 m high, and Meall Dearg at 952 m high.",
+        imageUrl: "https://images.unsplash.com/photo-1453225192584-ec69dd22dbc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YW9uYWNoJTIwZWFnYWNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+        id: 4,
+        title: "Zion National Park",
+        location: "Utah",
+        googleMapsUrl: "https://www.google.com/maps/place/Zion+National+Park/@37.2982022,-113.0263005,11z/data=!4m5!3m4!1s0x0:0x7c2e3a15aa3656f5!8m2!3d37.2982022!4d-113.0263005",
+        startDate: "12 Jan, 2019",
+        endDate: "24 Jan, 2019",
+        description: "Zion National Park is a southwest Utah nature preserve distinguished by Zion Canyon’s steep red cliffs. Zion Canyon Scenic Drive cuts through its main section, leading to forest trails along the Virgin River.",
+        imageUrl: "https://images.unsplash.com/photo-1520810760276-e2f31dee978f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8emlvbiUyMHBhcmt8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
     }
 ]
