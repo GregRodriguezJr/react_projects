13eb17f7d85601ce537b50fe9f25f28d335a4c3f
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-07 19:02:25 -0600

    added main directory, and header component

---
+++ travel_journal/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+
+        <h3>my travel journal</h3>
+    </div>
+  )
+}
+
+export default Header;
