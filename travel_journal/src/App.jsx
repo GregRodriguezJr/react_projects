58c3a98c59ffe58663ff5f5237c2f57c5ce99824
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-07 20:23:58 -0600

    made card component and tested output

--- travel_journal/src/App.jsx
+++ travel_journal/src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
+import Card from './components/Card';
 import Header from './components/Header';
+import tripsData from './data';
 
 function App() {
+
+  const cards = tripsData.map(trip => {
+    return (
+      <Card 
+        key={trip.id}
+        trip={trip}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Header />
+      <section>
+        {cards}
+      </section>
     </div>
   )
 }
