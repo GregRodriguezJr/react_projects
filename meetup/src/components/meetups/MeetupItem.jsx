27e11382ed2ee89ff89d6eb0db164d33c6dc5725
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-23 14:51:21 -0600

    created new components with css

---
+++ meetup/src/components/meetups/MeetupItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function MeetupItem() {
+  return (
+    <li>
+        <div>
+            <img src="" />
+        </div>
+        <div>
+            <h3></h3>
+            <address></address>
+            <p></p>
+        </div>
+        <div>
+            <button>To Favorites</button>
+        </div>
+    </li>
+  )
+}
+
+export default MeetupItem
