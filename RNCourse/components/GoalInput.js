b5b9d5532389313ec66fb1ce853a9c6eaa8a75be
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-25 15:48:05 -0600

    created modal for input component

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Modal } from 'react-native';
 import { useState } from 'react';
 
 function GoalInput(props) {
@@ -14,37 +14,50 @@ function GoalInput(props) {
     }
 
   return (
-    <View style={styles.inputContainer}>
-        <TextInput 
-          onChangeText={goalInputHandler} 
-          style={styles.textInput} 
-          placeholder='Your course goal'
-          value={enteredGoalText}
-        />
-        <Button 
-          onPress={addGoalHandler} 
-          title="Add Goal" 
-        />
-    </View>
+    <Modal visible={props.visible} animationType='slide'>
+        <View style={styles.inputContainer}>
+            <TextInput 
+                onChangeText={goalInputHandler} 
+                style={styles.textInput} 
+                placeholder='Your course goal'
+                value={enteredGoalText}
+            />
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button onPress={addGoalHandler} title="Add Goal" />
+                </View>
+                <View style={styles.button}>
+                    <Button title='Cancel' />
+                </View>
+            </View>
+        </View>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 24,
         borderBottomWidth: 1,
-        borderBottomColor: 'grey'
+        borderBottomColor: 'grey',
+        padding: 16
       },
       textInput: {
         borderWidth: 1,
         borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
+        width: '90%',
         padding: 8
+      },
+      buttonContainer: {
+        marginTop: 16,
+        flexDirection: 'row',
+      },
+      button: {
+        width: '30%',
+        marginHorizontal: 8
       }
 });
 
--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -3,11 +3,14 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 function GoalItem(props) {
 
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable 
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({pressed})=> pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -20,6 +23,9 @@ const styles = StyleSheet.create({
       },
       goalText: {
         color: '#fff'
+      },
+      pressedItem: {
+        opacity: 0.5
       }
 });
 
