c2ca16211e69d5de06930c7cae86c0641d692097
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-25 12:54:15 -0600

    created goal item component and moved code from app.js

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
 import { useState } from 'react';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -34,11 +35,7 @@ export default function App() {
           data={courseGoals} 
           renderItem={(itemData) => {
             itemData.index
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return (<GoalItem text={itemData.item.text} />);
           }}
         />
       </View>
@@ -70,14 +67,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5
-  },
-  goalItem: {
-    margin: 8,
-    borderRadius: 6,
-    backgroundColor: 'blue',
-    padding: 8
-  },
-  goalText: {
-    color: '#fff'
   }
 });
