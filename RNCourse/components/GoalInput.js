c2d929b065f3b7c061c58acd6af6edd1ed85e0d8
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-25 16:24:18 -0600

    added style to img and background

--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Button, TextInput, Modal } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Modal, Image } from 'react-native';
 import { useState } from 'react';
 
 function GoalInput(props) {
@@ -16,6 +16,7 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType='slide'>
         <View style={styles.inputContainer}>
+            <Image style={styles.image} source={require('../assets/favicon.png')}/>
             <TextInput 
                 onChangeText={goalInputHandler} 
                 style={styles.textInput} 
@@ -40,16 +41,17 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: 'grey',
-        padding: 16
+        padding: 16,
+        backgroundColor: '#311b6b'
       },
       textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
         width: '90%',
-        padding: 8
+        padding: 8,
+        color: '#120438',
+        borderRadius: 6
       },
       buttonContainer: {
         marginTop: 16,
@@ -58,6 +60,9 @@ const styles = StyleSheet.create({
       button: {
         width: '30%',
         marginHorizontal: 8
+      },
+      image: {
+        margin: 20
       }
 });
 
