2a6d53d64992b35938e51844bf3153cf9cdf1bc2
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-11-27 13:28:07 -0600

    completed sub heading

--- advisor/screens/HomeScreen.js
+++ advisor/screens/HomeScreen.js
@@ -16,12 +16,22 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-white flex-1 relative">
 
-        {/* First Section */}
+        {/* First section */}
         <View className="flex-row px-6 mt-8 items-center space-x-2">
             <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
                 <Text className="text-[#00BCC9] text-3xl font-semibold">GO</Text>
             </View>
             <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+        </View>
+
+        {/* Section section */}
+        <View className="px-6 mt-8 space-y-3">
+            <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+            <Text className="text-[#00BCC9] text-[36px] font-bold">Good moments</Text>
+
+            <Text className="text-[#3C6072]">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </Text>
         </View>
 
     </SafeAreaView>
