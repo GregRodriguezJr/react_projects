29827b241e6b6c0a547d9b40190697088442bd8e
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-15 15:47:54 -0600

    added filtered expenses to render list based on user selection

--- expense_tracker/src/components/expenses/Expenses.js
+++ expense_tracker/src/components/expenses/Expenses.js
@@ -5,25 +5,31 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2023');
-  
+  const [filteredYear, setFilteredYear] = useState("2023");
+
   const filterChangeHandler = (selectedYear) => {
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
-  }
+  };
+
+  const filteredExpenses = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <>
       <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((item) => 
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      )}
+        {filteredExpenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
       </Card>
     </>
   );
