70fe65e0a7ba27cdc03cc5f831218d7d5c154cdd
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-15 15:03:52 -0600

    set expenses dynamically by updating state

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,31 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 300,
+    date: new Date(2023, 1, 12),
+  },
+  {
+    id: "e2",
+    title: "Health Insurance",
+    amount: 400,
+    date: new Date(2023, 1, 20),
+  },
+  {
+    id: "e3",
+    title: "Mortgage",
+    amount: 1400,
+    date: new Date(2023, 1, 1),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 300,
-      date: new Date(2023, 1, 12),
-    },
-    {
-      id: "e2",
-      title: "Health Insurance",
-      amount: 400,
-      date: new Date(2023, 1, 20),
-    },
-    {
-      id: "e3",
-      title: "Mortgage",
-      amount: 1400,
-      date: new Date(2023, 1, 1),
-    },
-  ];
+
+  const [expenses, setExpenses] = useState(dummyExpenses);
 
   const addExpenseHandler = (expense) => {
-    console.log("App js ", expense);
+    setExpenses((prevExpenses)=> {
+      return [expense, ...prevExpenses];
+    })
   };
 
   return (
