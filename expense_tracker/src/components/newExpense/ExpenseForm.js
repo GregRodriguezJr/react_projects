05b761123784f3a5efb0b23173bc8f57be851751
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-14 18:32:12 -0600

    added useState hook to set expense form

--- expense_tracker/src/components/newExpense/ExpenseForm.js
+++ expense_tracker/src/components/newExpense/ExpenseForm.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [inputForm, setInputForm] = useState({
+        inputTitle:'',
+        inputAmount:'',
+        inputDate:''
+    });
+
+    const titleChangeHandler = (e) => {
+        e.preventDefault();
+        setInputForm((prevState)=> {
+        return {...prevState, inputTitle: e.target.value};
+        });
+    }
+
+    const amountChangeHandler = (e) => {
+        e.preventDefault();
+        setInputForm((prevState) => {
+            return {...prevState, inputAmount: e.target.value};
+        })
+    }
+
+    const dateChangeHandler = (e) => {
+        e.preventDefault();
+        setInputForm((prevState) => {
+            return {...prevState, inputDate: e.target.value};
+        })
+    }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2023-12-31" />
+          <input type="date" min="2023-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
