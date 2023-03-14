1a6c0eb6e40e4b061056ecfb469177ad6be19949
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-14 13:23:27 -0500

    created dummy products array to map over to created dynamic links

--- react_router/src/pages/Products.js
+++ react_router/src/pages/Products.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// call to get products from backend example
+const PRODUCTS = [
+    { id: "p1", title: "Product 1" },
+    { id: "p2", title: "Product 2" },
+    { id: "p3", title: "Product 3" },
+];
 
 const Products = () => {
     return (
         <>
             <h1>Products Page</h1>
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                {PRODUCTS.map((product) => (
+                    <li>
+                        <Link to={`/products/${product.id}`}>
+                            {product.title}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
