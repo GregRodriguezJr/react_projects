eab65869826f89747d9ef063face5090aeae6cec
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-14 13:13:58 -0500

    created product details component

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
 import Root from "./pages/Root";
 
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
         children: [
             { path: "/", element: <Home /> },
             { path: "/products", element: <Products /> },
+            { path: "/products/:productId", element: <ProductDetails /> },
         ],
     },
 ]);
