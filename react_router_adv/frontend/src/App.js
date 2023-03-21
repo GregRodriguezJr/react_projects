645cae543f201125f8b8d3c8f37d45d82fdbac54
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-20 20:17:38 -0500

    wrapped events with events navigation and updated links

--- react_router_adv/frontend/src/App.js
+++ react_router_adv/frontend/src/App.js
@@ -4,21 +4,27 @@ import EventsPage from "./pages/Events";
 import EventDetailsPage from "./pages/EventDetails";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import Root from "./pages/Root"
+import Root from "./pages/Root";
+import EventsRoot from "./pages/EventsRoot";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
         children: [
-            { path: "/", element: <HomePage /> },
-            { path: "/events", element: <EventsPage /> },
-            { path: "/events/:eventId", element: <EventDetailsPage /> },
-            { path: "/events/new", element: <NewEventPage /> },
-            { path: "/events/:eventId/edit", element: <EditEventPage /> },
-        ]
+            { path: "", element: <HomePage /> },
+            {
+                path: "events",
+                element: <EventsRoot />,
+                children: [
+                    { path: "", element: <EventsPage /> },
+                    { path: ":eventId", element: <EventDetailsPage /> },
+                    { path: "new", element: <NewEventPage /> },
+                    { path: ":eventId/edit", element: <EditEventPage /> },
+                ],
+            },
+        ],
     },
-    
 ]);
 
 function App() {
