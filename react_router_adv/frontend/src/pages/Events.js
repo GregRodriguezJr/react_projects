c3706d0118b426a30e1cea224359f4793e400e04
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-23 14:49:51 -0500

    implemented useLoaderData hook to render fetched data

--- react_router_adv/frontend/src/pages/Events.js
+++ react_router_adv/frontend/src/pages/Events.js
@@ -1,31 +1,14 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 const Events = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [fetchedEvents, setFetchedEvents] = useState();
-    const [error, setError] = useState();
-
-    useEffect(() => {
-        const fetchEvents = async () => {
-            setIsLoading(true);
-            
-            setIsLoading(false);
-        };
-        fetchEvents();
-    }, []);
+    const events = useLoaderData();
 
     return (
         <>
-            <div style={{ textAlign: "center" }}>
-                {isLoading && <p>Loading...</p>}
-                {error && <p>{error}</p>}
-            </div>
-            {!isLoading && fetchedEvents && (
-                <EventsList events={fetchedEvents} />
-            )}
+            <EventsList events={events} />
         </>
     );
 };
