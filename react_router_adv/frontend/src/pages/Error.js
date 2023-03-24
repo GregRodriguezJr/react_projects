43fd192650039360d87dc12fa64768e5725c34fc
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-24 12:25:19 -0500

    added page content component, added error handling for events

--- react_router_adv/frontend/src/pages/Error.js
+++ react_router_adv/frontend/src/pages/Error.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
 
 const Error = () => {
-    return <h1>Opps and error occured!</h1>;
+    const error = useRouteError();
+
+    let title = "An Error Occurred!";
+    let message = "Something went wrong...";
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message;
+    }
+
+    if (error.status === 404) {
+        title = "Not Found";
+        message = "Could not find resource or page";
+    }
+
+    return (
+        <>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
+        </>
+    );
 };
 
 export default Error;
--- react_router_adv/frontend/src/pages/Events.js
+++ react_router_adv/frontend/src/pages/Events.js
@@ -20,7 +20,10 @@ export const eventsLoader = async () => {
     const response = await fetch("http://localhost:8080/events");
     if (!response.ok) {
         // handle error
-        throw { message: "Could not "}
+        throw new Response(
+            JSON.stringify({ message: "Could not fetch events" }),
+            { status: 500 }
+        );
     } else {
         return response;
     }
