d323bef6d5fbbf45130f17d2e687f974e5581882
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-26 15:12:25 -0500

    created newEventAction for POST request

--- react_router_adv/frontend/src/App.js
+++ react_router_adv/frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage, { eventsLoader } from "./pages/Events";
 import EventDetailsPage, { eventDetailLoader } from "./pages/EventDetails";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import Root from "./pages/Root";
 import EventsRoot from "./pages/EventsRoot";
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
                                 path: "",
                                 element: <EventDetailsPage />,
                             },
-                            { path: "edit", element: <EditEventPage /> },
+                            { path: "edit", element: <EditEventPage />, action: newEventAction },
                         ],
                     },
                     { path: "new", element: <NewEventPage /> },
